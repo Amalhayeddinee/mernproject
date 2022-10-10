8cd6b914a6c2b8973a384eff483e81d8b605ff7b
amal hayeddine <hayeddineamel@gmail.com>
2022-10-10 11:39:00 +0100

    first commit

---
+++ products.js
@@ -0,0 +1,30 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    brand: "Apple",
+    desc: "6.1-inch display",
+    price: 999,
+    image: "https://images.bfmtv.com/RVbCxZc-13W-0GEbzNUR8CWZRBo=/4x3:1236x696/640x0/images/L-iPhone-13-Pro-d-Apple-1134667.jpg"  
+  },
+  {
+    id: 2,
+    name: "iPhone 12", 
+    brand: "Apple",
+    desc: "5.4-inch mini display",
+    price: 699,
+    image:
+    "https://images.bfmtv.com/RVbCxZc-13W-0GEbzNUR8CWZRBo=/4x3:1236x696/640x0/images/L-iPhone-13-Pro-d-Apple-1134667.jpg"  
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    brand: "Samsung",
+    desc: "6.5-inch display",
+    price: 399,
+    image:
+    "https://images.bfmtv.com/RVbCxZc-13W-0GEbzNUR8CWZRBo=/4x3:1236x696/640x0/images/L-iPhone-13-Pro-d-Apple-1134667.jpg"  
+  },
+];
+
+module.exports = products;
